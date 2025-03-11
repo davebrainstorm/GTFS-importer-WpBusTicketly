a911dd3280aa8c9409413fa1854ededb1e79aab1
davebrainstorm <davecurtindesign@gmail.com>
2025-03-11 09:05:42 +0700

    Update class-deactivator.php

--- includes/class-deactivator.php
+++ includes/class-deactivator.php
@@ -1,7 +1,41 @@
 <?php
 /**
- * Cleanup on deactivation
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    GTFS_Importer
+ * @subpackage GTFS_Importer/includes
  */
+
 class GTFS_Importer_Deactivator {
-    // Deactivation functionality
+
+    /**
+     * Deactivate the plugin.
+     *
+     * Performs necessary cleanup when the plugin is deactivated.
+     * Note: This is different from uninstallation - data is preserved.
+     *
+     * @since    1.0.0
+     */
+    public static function deactivate() {
+        // Clear any scheduled cron jobs
+        wp_clear_scheduled_hook('gtfs_importer_daily_update');
+        wp_clear_scheduled_hook('gtfs_importer_realtime_update');
+        
+        // Flush rewrite rules
+        flush_rewrite_rules();
+        
+        // Log deactivation
+        if (WP_DEBUG) {
+            error_log('GTFS Importer plugin deactivated');
+        }
+        
+        // Update plugin status in options
+        $settings = get_option('gtfs_importer_settings', array());
+        $settings['plugin_active'] = false;
+        $settings['deactivated_time'] = current_time('mysql');
+        update_option('gtfs_importer_settings', $settings);
+    }
 }
